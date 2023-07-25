072c4b08e8a31daffa80b53928602da5f331c5fc
levi <cgfeel@gmail.com>
2023-07-25 11:31:57 +0800

    补充头部

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import GithubCorner from '../components/github-corner/GithubCorner'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +17,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body 
+        className={inter.className}
+      >
+        <GithubCorner />
+        {children}
+      </body>
     </html>
   )
 }
