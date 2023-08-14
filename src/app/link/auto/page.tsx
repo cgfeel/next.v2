902951b38967047d4cbd8e06e33e11e6dcc2f628
levi <cgfeel@gmail.com>
2023-08-15 05:46:59 +0800

    添加刷新视图

--- src/app/link/auto/page.tsx
+++ src/app/link/auto/page.tsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 import { action } from "./action";
 
 // invalid
-export const dynamic = 'force-dynamic';
-export const fetchCache = 'force-no-store';
-export const revalidate = 0;
+// export const dynamic = 'force-dynamic';
+// export const fetchCache = 'force-no-store';
+// export const revalidate = 0;
 
 export default function Page() {
 
@@ -22,6 +22,12 @@ export default function Page() {
             <div>通过get请求跳转是不能刷新视图的，哪怕通过嘞中间请求清除了tag和path</div>
             <hr />
             <Link href={'/api/time/link'}>auto link</Link>
+            <hr />
+            <Link href="/link/fetch">auto fetch</Link>
+            <hr />
+            <a href="/api/time/link">api link by org</a>
+            <hr />
+            <a href="/link/fetch">fetch link by org</a>
             {/* invalid <Link href={`/api/time/link?${Date.now()}`}>auto link</Link>*/}
         </div>
     );
