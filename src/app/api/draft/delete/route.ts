87b6dde534c64aa8e364b1b28c5b5b4fd3c6625c
levi <cgfeel@gmail.com>
2023-08-19 21:49:30 +0800

    调整一下

--- src/app/api/draft/delete/route.ts
+++ src/app/api/draft/delete/route.ts
@@ -1,7 +1,7 @@
 import { draftMode } from "next/headers";
-import { redirect } from "next/navigation";
+import { NextResponse } from "next/server";
 
 export async function GET(Response: Request) {
     draftMode().disable();
-    redirect('/blog/draft');
+    NextResponse.redirect('/blog/draft');
 }
