ce3f144fec6a1a40e1e9806e4ebfe1a7bf6b6ab6
levi <cgfeel@gmail.com>
2023-08-20 01:55:04 +0800

    添加params

--- src/app/func/params/shop/[tag]/[item]/page.tsx
+++ src/app/func/params/shop/[tag]/[item]/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useParams } from "next/navigation";
-import styles from "../styles.module.css";
+import styles from "../../../../styles.module.css";
 
 export default function Page() {
     const params = useParams();
