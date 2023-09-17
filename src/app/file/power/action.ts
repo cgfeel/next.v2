14aa1dbe2081fabd60d1e9fb61096c3120c70c37
levi <cgfeel@gmail.com>
2023-09-17 21:00:27 +0800

    修正引用

--- src/app/file/power/action.ts
+++ src/app/file/power/action.ts
@@ -1,6 +1,5 @@
 'use server'
 
-import { revalidatePath } from "next/cache";
 import { cookies } from "next/headers"
 
 export const setPower: (login?: boolean) => Promise<void> = async (login = true) => {
